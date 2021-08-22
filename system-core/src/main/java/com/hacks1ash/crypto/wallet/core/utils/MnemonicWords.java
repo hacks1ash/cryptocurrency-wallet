f6c9de3cdf596bd2ec2618751aa17db9e5d1b583
Mikheil Maisuradze <miglesworkspace@gmail.com>
2021-08-22 15:35:55 -0400

    Fixed File Exception

--- system-core/src/main/java/com/hacks1ash/crypto/wallet/core/utils/MnemonicWords.java
+++ system-core/src/main/java/com/hacks1ash/crypto/wallet/core/utils/MnemonicWords.java
@@ -1,26 +1,18 @@
 package com.hacks1ash.crypto.wallet.core.utils;
 
-import org.bitcoinj.core.*;
-import org.bitcoinj.crypto.DeterministicKey;
-import org.bitcoinj.crypto.HDKeyDerivation;
-import org.bitcoinj.script.Script;
-import org.bitcoinj.script.ScriptBuilder;
-import org.bitcoinj.script.ScriptPattern;
-import org.bitcoinj.wallet.DeterministicSeed;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.time.Instant;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -30,19 +22,14 @@ public class MnemonicWords {
   private List<String> words;
 
   @PostConstruct
-  public void buildList() {
-    URL resource = getClass().getClassLoader().getResource("words.txt");
-    if (resource == null) {
-      throw new IllegalArgumentException("file not found!");
-    } else {
-      try {
-        File file = new File(resource.toURI());
-        words = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
-      } catch (IOException | URISyntaxException e) {
-        e.printStackTrace();
-      }
+  public void buildList() throws IOException {
+    InputStream resource = new ClassPathResource("words.txt").getInputStream();
+    try ( BufferedReader reader = new BufferedReader(
+      new InputStreamReader(resource)) ) {
+      words = reader.lines().collect(Collectors.toList());
+    } catch (IOException ex) {
+      ex.printStackTrace();
     }
-
   }
 
   public List<String> getWords() {
