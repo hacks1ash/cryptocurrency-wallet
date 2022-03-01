a14d2377ceec2b099b90c5549dae973049d508c4
Mikheil Maisuradze <mmaisu@odradek.ge>
2022-03-01 00:10:57 -0500

    Some config fixes

--- blockchain-integration/core/src/main/java/com/hacks1ash/crypto/wallet/blockchain/factory/UTXOProvider.java
+++ blockchain-integration/core/src/main/java/com/hacks1ash/crypto/wallet/blockchain/factory/UTXOProvider.java
@@ -1,14 +1,7 @@
 package com.hacks1ash.crypto.wallet.blockchain.factory;
 
-import com.hacks1ash.crypto.wallet.blockchain.litecoin.config.LitecoinMainNetParams;
-import com.hacks1ash.crypto.wallet.blockchain.litecoin.config.LitecoinRegTestParams;
-import com.hacks1ash.crypto.wallet.blockchain.litecoin.config.LitecoinTestNet3Params;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.params.MainNetParams;
-import org.bitcoinj.params.RegTestParams;
-import org.bitcoinj.params.TestNet3Params;
 
 @Getter
 @AllArgsConstructor
